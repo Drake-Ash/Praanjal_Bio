e29afe0f5eabe0ccf7d1f404f0dcc3301aac5bc4
Drake-Ash <ashwinv4444@gmail.com>
2018-04-08 13:02:41 +0530

    Added all the library files, added get_started

---
+++ app/src/main/java/com/demo/ash/demoapp/OTP_Verification.kt
@@ -0,0 +1,12 @@
+package com.demo.ash.demoapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class OTP_Verification : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp__verification)
+    }
+}
